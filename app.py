21f5924e87b3128c00168a02096c44ca56db47eb
thetechlaw60 <ejaylyn98@gmail.com>
2022-11-06 23:25:23 +0100

    commit

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 import os
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
 from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+
+
 
 
 
@@ -25,26 +26,26 @@ db.init_app(app)
         
        
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255),nullable=False) 
     last_name = db.Column(db.String(255),nullable=False) 
     email = db.Column(db.String(255),unique=True,nullable=False) 
     password= db.Column(db.String(255),nullable=False) 
     gender = db.Column(db.String(25),nullable=False) 
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    post = db.relationship('post', backref="user", passive_deletes=True )
+    posts = db.relationship('Post', backref="user", passive_deletes=True )
     
 def __repr__(self):
-    return f"User <{self.first_name}>"  
+    return f"User '{self.first_name}', '{self.last_name}'. '{self.email}' "  
 
 class Post(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
     
 def __repr__(self):
-    return f"Post('<{self.text}>', '<{self.date_create}>')"    
+    return f"Post('{self.text}', '{self.date_created}')"    
 
 login_manager = LoginManager()
 login_manager.login_view = "login"
@@ -61,7 +62,8 @@ def load_user(id):
 @app.route("/home")
 @login_required
 def home():
-    return render_template("home.html", user=current_user.email)
+    posts = Post.query.all()
+    return render_template("home.html", posts=posts, user=current_user.email)
 
 
 @app.route("/about")
@@ -77,9 +79,9 @@ def contact():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'post':
-        email =request.form.get('email')
-        password =request.form.get('password')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
         if user:
@@ -116,7 +118,7 @@ def register():
         if len(email) < 4:
             flash('Email is Invalid.', category='error')  
         else:
-            new_user = User(email=email, first_name=first_name, last_name=last_name, gender=gender, password=generate_password_hash(password1))          
+            new_user = User(email=email, first_name=first_name, last_name=last_name, gender=gender, password=generate_password_hash(password1, method='sha256'))          
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
@@ -125,13 +127,13 @@ def register():
                
             
     
-    return render_template('register.html', email=current_user)
+    return render_template('register.html', user=current_user)
 
 
 @app.route('/logout')
 @login_required
 def logout():
-    
+    logout_user()   
     return redirect(url_for('home'))
 
 
@@ -147,14 +149,40 @@ def create_post():
             new_post = Post(content=content, author=current_user.id)  
             db.session.add(new_post)
             db.session.commit()
-            flash('post created!', category='error')
+            flash('post created!', category='success')
         
             
             return redirect('home')
                
     return render_template('create_post.html', user=current_user)
 
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+@login_required
+def edit(id):
+    content_to_edit = Post.query.get_or_404(id)
+    if request.method == "POST":
+        content_to_edit.content = request.form.get("content")
+        try:
+            db.session.commit()
+            return redirect(url_for("home"))
+        except:
+            flash("There was a problem updating that post.", category="error")
+    return render_template('edit.html', content_to_edit=content_to_edit)
 
+
+@app.route("/delete/<int:id>")
+@login_required
+def delete(id):
+    post = Post.query.get_or_404(id)
+    
+
+    try:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for("home"))
+    except:
+        flash("There was a problem deleting that post. ", category="error")
+        
 
 
 
